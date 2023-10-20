7174060977f3ea342149ca97f9d6c0f9a57b7dfa
shiva <shivajangra303@gmail.com>
2023-10-20 13:11:52 +0530

    Setup layout - initially

---
+++ components/home/home.js
@@ -0,0 +1,7 @@
+function HomePage(){
+    return (
+        <div>Welcome to home page</div>
+    )
+}
+
+export default HomePage;
---
+++ pages/auth.js
@@ -0,0 +1,9 @@
+import AuthForm from '../components/auth/auth_form';
+
+function Auth(){
+    return (
+        <AuthForm/>
+      )
+}
+
+export default Auth;
